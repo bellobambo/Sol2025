6ea89b1aac19dca56b13768bb82c1e8d0632949e
bellobambo <bellobambo21@gmail.com>
2025-06-04 11:11:13 +0100

    Solidity2025

---
+++ contracts/ControlStructure.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+
+
+contract ControlStructure{
+  function compareNumbers(uint256 _number1, uint256 _number2) external returns (string memory){
+    
+  }
+
+}
